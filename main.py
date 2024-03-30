17ede87b5053a448be98ff9ce579e70edad09ca6
Leonardo de Freitas <leonardo.freitas777x@gmail.com>
2024-03-30 18:42:15 -0500

    Annoying git

--- main.py
+++ main.py
@@ -294,7 +294,6 @@ while game_loop:
     else:
       score -= 15
       
-
   if CDS == True:  
 
     if ((screen_scroll > 2 or screen_scroll < -4463) and (screen_scroll <= 145 or screen_scroll >= -4462) and cargo_y > 232) or terrain_mask.overlap(cargo_mask,(cargo_x + 228 - screen_scroll, cargo_y)) or terrain_mask.overlap(cargo_mask,(cargo_x + 228 - screen_scroll + 4608, cargo_y)):
